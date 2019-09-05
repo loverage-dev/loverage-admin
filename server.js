551d7cac1b64ea823c67f5cc6f854a4313b0994c
zetton110 <zetton110@gmail.com>
2019-09-05 23:23:11 +0900

    ログイン機能の追加

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const basicAuth = require('basic-auth-connect');
 const port = process.env.PORT || 8080;
 const app = express();
+var username = process.env.BASIC_AUTH_USERNAME;
+var password = process.env.BASIC_AUTH_PASSWORD;
+
+if (username && password) {
+    app.use(basicAuth(username, password));
+}else{
+    app.use(basicAuth("love", "average"));
+}
+
 app.use(express.static(__dirname + "/dist/"));
 app.listen(port);
